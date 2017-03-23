df3d09c4501f1bacb8b5a40dcde7fc36522a0f9a
Luca Mezzalira <mezzaliraluca@gmail.com>
2017-03-23 22:18:20 +0000

    removed comment

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import _ from 'lodash';
 function component () {
   var element = document.createElement('div');
 
-  /* lodash is required for the next line to work */
   element.innerHTML = _.join(['Hello','webpack'], ' ');
 
   return element;
